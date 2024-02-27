76427ff623fd160f4bdd539377ce6b67f9124b35
Avelia Diva Zahra <avelia.diva@ui.ac.id>
2024-02-27 21:38:53 +0700

    double jump

--- scenes/Player.gd
+++ scenes/Player.gd
@@ -1,87 +1,93 @@
 extends KinematicBody2D
 
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-
-# Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
-
-export (int) var speed = 400 # diexport biar keliatan di inspektornya. biar ez lebi fleksibel
+export (int) var speed = 400 #export
 export (int) var GRAVITY = 1200
 export (int) var jump_speed = -600
 
-#var right_counter = 0
-#var left_counter = 0
 
 var last_move_right_press_time = 0
+var last_move_left_press_time = 0
 var double_tap_threshold = 0.15
+
+var max_jumps = 2
+var jumps_remaining = max_jumps
 
 const UP = Vector2(0,-1)
 
 var velocity = Vector2()
 
-#var direction = Vector2()
+func double_tap(direction,last_move_time):
+	var current_time = OS.get_ticks_msec() / 1000.0
+	
+	if Input.is_action_just_pressed(direction):
+		print(current_time)
+		if current_time - last_move_time < double_tap_threshold:
 
+			print("Double tap detected!") 
+			return true
+	
+	return false
+	
 func get_input():
 	velocity.x = 0
 	
+	if is_on_floor():
+		jumps_remaining = max_jumps
 	
-	if is_on_floor() and Input.is_action_just_pressed('up'):
+	if Input.is_action_just_pressed("up") and jumps_remaining > 0:
 		velocity.y = jump_speed
+		jumps_remaining -= 1
+	
+	#if is_on_floor() and Input.is_action_just_pressed('up'):
 		
+	#	velocity.y = jump_speed
+		
+		
+	#	var current_time = OS.get_ticks_msec() / 1000.0
 		# https://www.youtube.com/watch?v=xweO2LCx_Yc
 		
 	if Input.is_action_pressed("right"):
 		
-		#if Input.is_action_just_pressed("right"): # deteksi sekali tekan -> GA IMMIDIATE WKWWKKWWKWK SALAH, kayaknya main just realeased, OH MAKE TIMER
-		#	right_counter += 1
-		#	print(right_counter)
-		
-		#if right_counter == 2:
-		#	speed = 600
-		#	right_counter = 0
-		#else:
-		#	speed = 400
-		var current_time = OS.get_ticks_msec() / 1000.0
-		if Input.is_action_just_pressed("right"):
-			  # Convert to seconds
-			print(current_time)
-			if current_time - last_move_right_press_time < double_tap_threshold:
-				# Double tap detected
-				print("Double tap detected!") # mantab, integrate bareng ama is just
-				speed = 600 #bejir la kekiri juga keefek wkwk
-		
+		var is_double = double_tap("right",last_move_right_press_time)
+		if is_double:
+			speed = 1000 
 		
 		velocity.x += speed
+		
+		var current_time = OS.get_ticks_msec() / 1000.0
 		last_move_right_press_time = current_time
-		#direction = Vector2.RIGHT
+
 	if Input.is_action_just_released("right"):
 		speed = 400
 		
-	# PERFEK!!
-	
+
 	if Input.is_action_pressed("left"):
+		
+		var is_double = double_tap("left",last_move_left_press_time) 
+		if is_double:
+			speed = 1000
+			
 		velocity.x -= speed
-		#direction = Vector2.LEFT
+		
+		var current_time = OS.get_ticks_msec() / 1000.0
+		last_move_left_press_time = current_time
+		
+	
+	if Input.is_action_just_released("left"):
+		speed = 400
 		
 	# Crouching
 	if Input.is_action_just_pressed("down"):
 		speed = 100
 	if Input.is_action_just_released("down"):
 		speed = 400
-		
-	#if Input.hold
 	
-	#velocity.x = direction.x * speed
+	
+	# next idea, roll? slide? shoot? punch?
 	
 func _physics_process(delta):
 	velocity.y += delta * GRAVITY
